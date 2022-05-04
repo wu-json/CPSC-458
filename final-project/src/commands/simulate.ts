34082d462b033907ec395981832b170e1427c90f
wu-json <jason.wu2153@gmail.com>
2022-05-04 00:36:52 -0400

    feat: more setup

--- final-project/src/commands/simulate.ts
+++ final-project/src/commands/simulate.ts
@@ -1,4 +1,5 @@
 import { Command } from "commander";
+import dataSource from "../connections/typeorm";
 
 const simulate = new Command()
   .command("simulate")
@@ -6,10 +7,23 @@ const simulate = new Command()
   .argument("[battles]", "How many simulated battles to run")
   .action(async (battles: string) => {
     try {
+      /**
+       * Connect to Postgres
+       */
+      console.log("---------------------------------");
+      console.log("Initializing data source...");
+
+      await dataSource.initialize();
+
+      console.log("Data source initialized.");
+      console.log("---------------------------------");
+
       const battlesToSimulate = battles ? parseInt(battles) : 100;
       console.log(`Simulating ${battlesToSimulate} battles.`);
+      process.exit(0);
     } catch (error) {
       console.error(`Something went wrong: ${error}`);
+      process.exit(1);
     }
   });
 
